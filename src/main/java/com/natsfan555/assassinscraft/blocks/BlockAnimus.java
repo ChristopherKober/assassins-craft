bc14369abda0e0a01656df3c0b4892dd415f62c5
ChristopherKober <christopherkober2018@u.northwestern.edu>
2016-07-11 14:53:15 -0700

    Minor bug fixes for animus tile entity
    Fixed a couple of bugs relating to the animus tile entity. Block still
    crashes on left click.

--- src/main/java/com/natsfan555/assassinscraft/blocks/BlockAnimus.java
+++ src/main/java/com/natsfan555/assassinscraft/blocks/BlockAnimus.java
@@ -5,41 +5,19 @@ import javax.annotation.Nullable;
 
 import com.natsfan555.assassinscraft.tileentity.TileEntityAnimus;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.EnumPushReaction;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Biomes;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import scala.Console;
 
-public class BlockAnimus extends Block implements ITileEntityProvider{
+public class BlockAnimus extends BlockContainer {
 
     public BlockAnimus()
     {
@@ -49,10 +27,20 @@ public class BlockAnimus extends Block implements ITileEntityProvider{
     }
 
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
-    	TileEntityAnimus tect = (TileEntityAnimus) worldIn.getTileEntity(pos);
-    	tect.addPlayer(playerIn.getName());
-    	System.out.println(tect.playerHistory);
-		playerIn.addChatMessage(new TextComponentString("Added cordinate cache to tile entity"));
+    	
+    	TileEntity tect = worldIn.getTileEntity(pos);
+    	
+    	if (tect instanceof TileEntityAnimus) {
+    		
+    		((TileEntityAnimus) tect).addPlayer(playerIn.getName());
+        	System.out.println(((TileEntityAnimus) tect).playerHistory);
+    		playerIn.addChatMessage(new TextComponentString("Added Player to List"));
+    		
+    		playerIn.displayGui((TileEntityAnimus)tect);
+    		
+    		
+    	}
+    	
     	return true;
     }
 
--- src/main/java/com/natsfan555/assassinscraft/tileentity/TileEntityAnimus.java
+++ src/main/java/com/natsfan555/assassinscraft/tileentity/TileEntityAnimus.java
@@ -4,32 +4,28 @@ package com.natsfan555.assassinscraft.tileentity;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.Packet;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.IInteractionObject;
 import net.minecraft.world.World;
 
-public class TileEntityAnimus extends TileEntity {
+public class TileEntityAnimus extends TileEntity implements IInteractionObject {
 	
 	public List<String> playerHistory = new ArrayList<String>();
 
 	public void addPlayer(String playerName) {
+		if (playerHistory.contains(playerName)) { return; }
 		playerHistory.add(playerName);
 	}
-
-//    public int decrement() {
-//        counter--;
-//        //markDirty();
-//        return counter;
-//    }
-//
-//    public int increment() {
-//        counter++;
-//        //markDirty();
-//        return counter;
-//    }
 
     @Override
     public void readFromNBT(NBTTagCompound comp) {
@@ -57,5 +53,35 @@ public class TileEntityAnimus extends TileEntity {
     	compound.setTag("MyStringList", tagList);
     	return compound;
     }
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasCustomName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ITextComponent getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGuiID() {
+		// TODO Auto-generated method stub
+		return "AssassinsCraft:Animus";
+	}
     
 }
